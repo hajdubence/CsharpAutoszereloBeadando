010b5cb5a9a8b301db9bbef7515ce065164391e6
Hajdu Bence <hajdubence2001@gmail.com>
2022-01-03 20:39:32 +0100

    first commit

---
+++ Autoszerelo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Autoszerelo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Szerelő!");
+            Console.WriteLine("Elromlott az autó!");
+        }
+    }
+}
