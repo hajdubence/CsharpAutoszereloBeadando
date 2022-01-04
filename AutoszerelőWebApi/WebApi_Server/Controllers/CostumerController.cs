ef49d7f23ace4c8e92d00deb3ebcf83229b35341
György David <gyorgydavid2000@gmail.com>
2022-01-04 19:49:22 +0200

    Repositoies and CRUD methods to Customer Controller added

--- AutoszerelőWebApi/WebApi_Server/Controllers/CostumerController.cs
+++ AutoszerelőWebApi/WebApi_Server/Controllers/CostumerController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApi_Common.Models;
+using WebApi_Server.Repositories;
 
 namespace WebApi_Server.Controllers
 {   
@@ -10,7 +11,83 @@ namespace WebApi_Server.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Customer>> Get()
         {
-            return Ok(new[] {new Customer {Name = "Jhon", CarType="Audi", LicensePlate="ABC123", Problem="Nem megy",Status="Felvett", DateOfRecording = DateTime.Now } });
+            var customers = CustomerRepository.GetCustomers();
+            return Ok(customers);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Customer> Get(int id)
+        {
+            var customers = CustomerRepository.GetCustomers();
+
+            var customer = customers.FirstOrDefault(x => x.Id == id);
+            if (customer != null)
+            {
+                return Ok(customer);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]Customer customer)
+        {
+            var customers = CustomerRepository.GetCustomers().ToList();
+            customer.Id = GetNewId(customers);
+            customers.Add(customer);
+
+            CustomerRepository.StoreCustomers(customers);
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Customer customer)
+        {
+            var customers = CustomerRepository.GetCustomers().ToList();
+
+            var customerToUpdate = customers.FirstOrDefault(c => c.Id == customer.Id);
+            if (customerToUpdate != null)
+            {
+                customerToUpdate.Name = customer.Name;
+                customerToUpdate.CarType = customer.CarType;
+                customerToUpdate.LicensePlate = customer.LicensePlate;
+                customerToUpdate.Problem = customer.Problem;
+                customerToUpdate.Status = customer.Status;
+
+                CustomerRepository.StoreCustomers(customers);
+                return Ok();
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var customers = CustomerRepository.GetCustomers().ToList();
+            var customerToDelete = customers.FirstOrDefault(c => c.Id == id);
+            if (customerToDelete != null)
+            {
+                customers.Remove(customerToDelete);
+
+                CustomerRepository.StoreCustomers(customers);
+                return Ok();
+            }
+
+            return NotFound();
+        }
+
+        private long GetNewId(IEnumerable<Customer> customers)
+        {
+            long newId = 0;
+            foreach (var customer in customers)
+            {
+                if (customer.Id > newId)
+                {
+                    newId = customer.Id;
+                }
+            }
+            return newId + 1;
         }
     }
 }
---
+++ AutoszerelőWebApi/WebApi_Server/Repositories/CustomerRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using WebApi_Common.Models;
+
+namespace WebApi_Server.Repositories
+{
+    public static class CustomerRepository
+    {
+        private const string filename = "Customer.json";
+
+        public static IEnumerable<Customer> GetCustomers()
+        {
+            if (File.Exists(filename))
+            {
+                var rawData = File.ReadAllText(filename);
+                var customers = JsonSerializer.Deserialize<IEnumerable<Customer>>(rawData);
+                return customers;
+            }
+
+            return new List<Customer>();
+        }
+
+        public static void StoreCustomers(IEnumerable<Customer> customers)
+        {
+            var rawData = JsonSerializer.Serialize(customers);
+            File.WriteAllText(filename, rawData);
+        }
+    }
+}
