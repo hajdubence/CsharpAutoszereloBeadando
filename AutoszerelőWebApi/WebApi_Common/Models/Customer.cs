83ae585b834405ebba0a804648876306cfd8aa1a
György David <gyorgydavid2000@gmail.com>
2022-01-04 18:44:02 +0200

    AutoszereloWepApi added to project

---
+++ AutoszerelőWebApi/WebApi_Common/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi_Common.Models
+{
+    public class Customer
+    {
+        
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string CarType { get; set; }
+        public string LicensePlate { get; set; }
+        public string Problem { get; set; }
+        public DateTime DateOfRecording { get; set; }
+        public string Status { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {CarType} {LicensePlate} - {Problem} {DateOfRecording} {Status}";
+        }
+    }
+}
---
+++ AutoszerelőWebApi/WebApi_Server/Controllers/CostumerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi_Common.Models;
+
+namespace WebApi_Server.Controllers
+{   
+    [ApiController]
+    [Route("api/customer")]
+    public class CostumerController:Controller
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> Get()
+        {
+            return Ok(new[] {new Customer {Name = "Jhon", CarType="Audi", LicensePlate="ABC123", Problem="Nem megy",Status="Felvett", DateOfRecording = DateTime.Now } });
+        }
+    }
+}
