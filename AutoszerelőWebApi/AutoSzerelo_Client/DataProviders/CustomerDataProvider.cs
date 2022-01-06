cc0d933d10ddf75947dbfb01d78bc2590547542c
György David <gyorgydavid2000@gmail.com>
2022-01-06 18:25:41 +0200

    Autoszerelo client added

---
+++ AutoszerelőWebApi/AutoSzerelo_Client/DataProviders/CustomerDataProvider.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi_Common.Models;
+
+namespace AutoSzerelo_Client.DataProviders
+{
+    class CustomerDataProvider
+    {
+        private const string _url = "http://localhost:5000/api/customer";
+
+        public static IEnumerable<Customer> GetCustomers()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(_url).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var rawData = response.Content.ReadAsStringAsync().Result;
+                    var customers = JsonConvert.DeserializeObject<IEnumerable<Customer>>(rawData);
+                    return customers;
+                }
+
+                throw new InvalidOperationException(response.StatusCode.ToString());
+            }
+        }
+
+        public static void UpdateCustomer(Customer customer, long id)
+        {
+            using (var client = new HttpClient())
+            {
+                var rawData = JsonConvert.SerializeObject(customer);
+                var content = new StringContent(rawData, Encoding.UTF8, "application/json");
+
+                var response = client.PutAsync($"{_url}/{id}", content).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException(response.StatusCode.ToString());
+                }
+            }
+        }
+    }
+}
