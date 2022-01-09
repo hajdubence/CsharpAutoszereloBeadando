3642b86996b38d7aaceed6a2bfc38772202d050c
Hajdu Bence <hajdubence2001@gmail.com>
2022-01-09 20:02:46 +0100

    Added Iroda_Client

---
+++ Iroda_Client/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using AutoSzerelo_Common.DataProviders;
+using AutoSzerelo_Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Iroda_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateRepairDataGrid();
+        }
+
+        private void RepairDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs args)
+        {
+            var selectedRepair = RepairDataGrid.SelectedItem as Repair;
+
+            if (selectedRepair != null)
+            {
+                /*var window = new RepairInfoWindow(selectedRepair);
+                if (window.ShowDialog() ?? false)
+                {
+                    RepairDataGrid_Update();
+                }*/
+
+                RepairDataGrid.UnselectAll();
+            }
+
+        }
+        public void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateRepairDataGrid();
+        }
+
+        private void UpdateRepairDataGrid()
+        {
+            var repairs = RepairDataProvider.GetRepairs().ToList();
+            RepairDataGrid.DataContext = repairs;
+        }
+
+    }
+}
