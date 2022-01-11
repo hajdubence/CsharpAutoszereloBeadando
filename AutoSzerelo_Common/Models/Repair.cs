a06e6078a013c78649a9161d81d72bdef5ecd606
Hajdu Bence <hajdubence2001@gmail.com>
2022-01-11 21:25:16 +0100

    Added validation functions

--- AutoSzerelo_Common/Models/Repair.cs
+++ AutoSzerelo_Common/Models/Repair.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AutoSzerelo_Common.Models
@@ -40,6 +41,32 @@ namespace AutoSzerelo_Common.Models
 
                 default: return $"{CustomerName} {CarType} {CarLicensePlate} - {Problem} {DateOfRecording} {Status}";
             }
+        }
+
+        public bool ValidateCustomerName() {
+            if (String.IsNullOrWhiteSpace(CustomerName))
+                return false;
+            return Regex.IsMatch(CustomerName, "^[A-Za-z\\s]+$");
+        }
+
+        public bool ValidateCarType()
+        {
+            if (String.IsNullOrWhiteSpace(CarType))
+                return false;
+            return Regex.IsMatch(CarType, "^[A-Za-z\\s]+$");
+        }
+
+        public bool ValidateCarLicensePlate()
+        {
+            if (String.IsNullOrWhiteSpace(CarLicensePlate))
+                return false;
+            if (Regex.IsMatch(CarLicensePlate, "^[A-Z]{3}[-][0-9]{3}$"))
+                return true;
+            return false;
+        }
+        public bool ValidateProblem()
+        {
+            return !String.IsNullOrWhiteSpace(Problem);
         }
     }
 }
